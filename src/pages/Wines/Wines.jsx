6837831fd93466fa3250a7b82cd5a1fbacd976b6
Nicolas Varone <44966886+nicolasvarone@users.noreply.github.com>
2023-08-02 13:32:40 +0200

    Change prices list

--- src/pages/Wines/Wines.jsx
+++ src/pages/Wines/Wines.jsx
@@ -7,7 +7,7 @@ import {
 } from '../../components/Container/Container.styled'
 import { Text, WrapperLink, WrapperButton } from './Wines.styled'
 import RangeWines from '../../components/RangeWines/RangeWines'
-import List from '../../images/liste_des_prix_2022.pdf'
+import List from '../../images/liste-des-prix-2023.pdf'
 import Button from '../../components/Button/Button'
 
 const Wines = () => {
